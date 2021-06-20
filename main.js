ae48f6ee364a2a874597869eaaa054f2463cb26e
shouryasingroha2008 <72244139+shouryasingroha2008@users.noreply.github.com>
2021-06-20 12:30:40 +0530

    Update main.js

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ function setup(){
     canvas=createCanvas(640,420);
     canvas.center();
     objectDetector=ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML="Status : Detecting Objects";
 }
 
 function modelLoaded(){
